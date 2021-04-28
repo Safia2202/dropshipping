91348f16cadb2f6c60d651c69617fbe31231d9ba
Cherrycherry2202 <huongnguyen2202@gmail.com>
2021-04-28 21:08:43 +0900

    add readme

---
+++ src/test/java/dropshipping/com/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dropshipping.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
